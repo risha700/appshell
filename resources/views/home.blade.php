faf4a5f6b5abd4a166a8ff9aedf3b8a74d018c65
Ahmed Shehab <ahbox@outlook.com>
2018-07-12 19:52:29 -0500

    change dashboard view

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,7 +7,7 @@
 
         <div class="uk-card uk-card-primary uk-card-body uk-width-1-2@m uk-align-center">
             <h3 class="uk-card-title">Dashboard</h3>
-            <p>You are logged in!</p>
+            <p>Welcome back {{auth()->user->name}}</p>
         </div>
 
     </div>
